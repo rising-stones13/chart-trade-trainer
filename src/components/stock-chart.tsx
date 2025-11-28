de9773991751d1b12a49cafd162dc67e3aef80be
イシイタカオ <rising.stones13@gmail.com>
2025-11-28 15:01:31 +0000

    もう少し暗いピンク、もう少し暗い水色にしてください

--- src/components/stock-chart.tsx
+++ src/components/stock-chart.tsx
@@ -141,7 +141,7 @@ export function StockChart({
 
     // Update data
     candleSeriesRef.current.setData(chartData);
-    const volumeData = chartData.map(d => ({ time: d.time, value: d.volume, color: d.close >= d.open ? '#FFC0CB' : '#87CEEB' }));
+    const volumeData = chartData.map(d => ({ time: d.time, value: d.volume, color: d.close >= d.open ? '#EC407A' : '#4FC3F7' }));
     volumeSeriesRef.current.setData(volumeData);
 
     Object.values(maConfigs).forEach(config => {
